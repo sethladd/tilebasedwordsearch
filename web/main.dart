5596e80eb2b4f408d0befd043a3f58222e38a977
Timothy Armstrong <armstrong.timothy@gmail.com>
2013-03-13 15:56:32 -0700

    Moving drawing

--- web/main.dart
+++ web/main.dart
@@ -4,6 +4,7 @@ import 'package:game_loop/game_loop.dart';
 import 'package:asset_pack/asset_pack.dart';
 import 'package:tilebasedwordsearch/dictionary.dart';
 import 'package:tilebasedwordsearch/game.dart';
+import 'package:tilebasedwordsearch/board_view.dart';
 
 CanvasElement _canvasElement;
 GameLoop _gameLoop;
@@ -23,79 +24,6 @@ void startNewGame() {
 bool f = true;
 void gameUpdate(GameLoop gameLoop) {
   // game.tick(gameLoop.dt);
-}
-
-class TileCoord {
-  num x, y;
-  TileCoord(this.x, this.y);
-}
-
-class BoardView {
-  int WIDTH = 320;
-  int HEIGHT = 320;
-
-  num tileSize;
-  num gapSize;
-
-  CanvasElement canvas;
-
-  // TODO: Set these.
-  String defaultColor;
-  String selectedColor;
-
-  BoardView(CanvasElement this.canvas) {
-    init();
-  }
-
-  void init() {
-    canvas.width = WIDTH;
-    canvas.height = HEIGHT;
-
-    var constraint = min(WIDTH, HEIGHT);
-    tileSize = constraint / 4.75;
-    gapSize = tileSize * 0.25;
-
-  }
-
-  TileCoord getTileCoord(int row, int column) {
-    num x = column * (tileSize + gapSize);
-    num y = row * (tileSize + gapSize);
-    return new TileCoord(x, y);
-  }
-
-  void drawTile(String letter, int score) {
-    // TODO.
-  }
-
-  void render() {
-    var c = canvas.context2d;
-
-    // Clear canvas.
-    c.clearRect(0, 0, WIDTH, HEIGHT);
-
-    // Loop through the tiles and draw each one.
-    for (int i = 0; i < 4; i++) { // each row
-      for (int j = 0; j < 4; j++) { // each column
-        c.fillStyle = '#f00';
-
-        var coord = getTileCoord(i, j);
-        c.fillRect(coord.x, coord.y, tileSize, tileSize);
-
-        c.fillStyle = '#000';
-        c.font = '${(tileSize).floor()}px/${(tileSize).floor()} sans-serif';
-        c.textBaseline = 'middle';
-
-        var text = '$i';
-        var width = c.measureText(text).width.clamp(0, tileSize);
-
-        var textOffset = (tileSize - width) / 2;
-
-        c.fillText(text, coord.x + textOffset, coord.y + tileSize * 0.5);
-      }
-    }
-
-
-  }
 }
 
 void gameRender(GameLoop gameLoop) {
