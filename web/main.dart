25693ff6945014712c30cb6a8552b16a3126f701
John McCutchan <john@johnmccutchan.com>
2013-03-13 14:02:43 -0700

    GameLoop and AssetManager hooked up
    Signed-off-by: John McCutchan <john@johnmccutchan.com>

--- web/main.dart
+++ web/main.dart
@@ -2,6 +2,27 @@ import 'dart:html';
 import 'package:game_loop/game_loop.dart';
 import 'package:asset_pack/asset_pack.dart';
 
+CanvasElement _canvasElement;
+GameLoop _gameLoop;
+AssetManager assetManager = new AssetManager();
+
+bool f = true;
+void gameUpdate(GameLoop gameLoop) {
+  // Game logic goes here.
+  if (f) {
+    print(assetManager['game.dictionary']);
+    f = false;
+  }
+}
+
+void gameRender(GameLoop gameLoop) {
+  // Paint here.
+}
+
 main() {
-    print('main');
+  _canvasElement = query('#frontBuffer');
+  _gameLoop = new GameLoop(_canvasElement);
+  _gameLoop.onUpdate = gameUpdate;
+  _gameLoop.onRender = gameRender;
+  assetManager.loadPack('game', 'assets.pack').then((_) => _gameLoop.start());
 }
