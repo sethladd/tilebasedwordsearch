43f0d1ae59f9d65059f952d44a5b06909b04bc31
Seth Ladd <sethladd@google.com>
2013-03-16 17:11:57 -0700

    move solver to discrete library

--- bin/solver.dart
+++ bin/solver.dart
@@ -1,55 +1,15 @@
-library solver;
-
-import 'dart:math';
+import 'package:tilebasedwordsearch/solver.dart';
 import 'dart:io';
 
-final Map words = new Map();
-List<List> grid;
-final List<List> visited = new List.generate(4, (_) => new List(4));
-final Map found = new Map();
-
-solve(int x, int y, [String word = '']) {
-  visited[x][y] = true;
-  
-  String newWord = '${word}${grid[x][y]}';
-  
-  if (words.containsKey(newWord)) {
-    found[newWord] = true;
-  }
-  
-  for (var _x = -1; _x < 2; _x++) {
-    var nX = x + _x;
-    if (nX < 0 || nX > 3) continue;
-    for (var _y = -1; _y < 2; _y++) {
-      if (_x == 0 && _y == 0) continue;
-      var nY = y + _y;
-      if (nY < 0 || nY > 3) continue;
-      if (visited[nX][nY] == true) continue;
-      solve(nX, nY, newWord);
-    }
-  }
-  
-  visited[x][y] = false;
-}
-
-findAll() {
-  for (var x = 0; x < 4; x++) {
-    for (var y = 0; y < 4; y++) {
-      solve(x, y);
-    }
-  }
-  
-  print('done!');
-}
-
 main() {
-  grid = const [
+  const List<List<String>> grid = const [
     const ['A', 'B', 'C', 'D'],
     const ['E', 'F', 'G', 'H'],
     const ['I', 'J', 'K', 'L'],
     const ['M', 'N', 'O', 'P']
   ];
   
+  Map words = new Map();
   Directory pwd = new File(new Options().script).directorySync();
   new File('${pwd.path}/../web/assets/dictionary.txt')
       .readAsLinesSync()
@@ -57,9 +17,9 @@ main() {
   print(words.length);
   
   var sw = new Stopwatch()..start();
-  findAll();
+  Iterable<String> results = findAll(grid, words);
   sw.stop();
   
-  print(found.keys.toList());
+  print(results.toList());
   print('Found in ${sw.elapsedMilliseconds} ms');
 }
---
+++ lib/solver.dart
@@ -0,0 +1,46 @@
+library solver;
+
+import 'dart:math';
+import 'dart:io';
+
+Map _words;
+List<List> _grid;
+final List<List> _visited = new List.generate(4, (_) => new List.filled(4, false));
+final Map _found = new Map();
+
+_solve(int x, int y, [String word = '']) {
+  _visited[x][y] = true;
+  
+  String newWord = '${word}${_grid[x][y]}';
+  
+  if (_words.containsKey(newWord)) {
+    _found[newWord] = true;
+  }
+  
+  for (var _x = -1; _x < 2; _x++) {
+    var nX = x + _x;
+    if (nX < 0 || nX > 3) continue;
+    for (var _y = -1; _y < 2; _y++) {
+      if (_x == 0 && _y == 0) continue;
+      var nY = y + _y;
+      if (nY < 0 || nY > 3) continue;
+      if (_visited[nX][nY] == true) continue;
+      _solve(nX, nY, newWord);
+    }
+  }
+  
+  _visited[x][y] = false;
+}
+
+Iterable<String> findAll(List<List<String>> grid, Map words) {
+  _grid = grid;
+  _words = words;
+  
+  for (var x = 0; x < 4; x++) {
+    for (var y = 0; y < 4; y++) {
+      _solve(x, y);
+    }
+  }
+  
+  return _found.keys;
+}
