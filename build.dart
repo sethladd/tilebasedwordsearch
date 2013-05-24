09256c41e47d5e0a110709c27ffa4ecb6454fd21
Seth Ladd <sethladd@google.com>
2013-05-24 11:24:12 -0700

    build copies assets

--- build.dart
+++ build.dart
@@ -1,7 +1,12 @@
 import 'package:web_ui/component_build.dart';
 import 'dart:io';
+import 'dart:async';
 
 void main() {
-  build(new Options().arguments, ['web/index.html']);
+  Future task = build(new Options().arguments, ['web/index.html']);
+  
+  task
+      .then((_) => Process.run('cp', ['-R', 'web/assets', 'web/out']))
+      .then((_) => print('All done'));
 }
 
---
+++ lib/components/high_scores.dart
@@ -0,0 +1,5 @@
+import 'package:web_ui/web_ui.dart';
+
+class HighScores extends WebComponent {
+  
+}
