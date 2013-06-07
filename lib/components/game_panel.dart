70b2717e1aa7027d9bb00330df9cf6d3c8d6fd17
John McCutchan <john@johnmccutchan.com>
2013-06-07 15:22:53 -0700

    Keyboard input and fix QU in score calculation.
    Signed-off-by: John McCutchan <john@johnmccutchan.com>

--- lib/components/game_panel.dart
+++ lib/components/game_panel.dart
@@ -20,7 +20,7 @@ class GamePanel extends WebComponent {
   CanvasElement _canvasElement;
   ButtonElement _pauseButton;
   ButtonElement _endButton;
-  
+
   @override
   inserted() {
     _pauseButton = query('#pause');
@@ -33,12 +33,17 @@ class GamePanel extends WebComponent {
     _gameLoop.onRender = gameRender;
     _gameLoop.onTouchStart = gameTouchStart;
     _gameLoop.onTouchEnd = gameTouchEnd;
-    
+    _gameLoop.keyboard.interceptor = keyboardEventInterceptor;
     enableButtons();
-    
+
     startNewGame();
   }
-  
+
+  @override
+  removed() {
+    _gameLoop.keyboard.interceptor = null;
+  }
+
   void startNewGame() {
     game = new Game(dictionary, _canvasElement, _gameLoop, letterAtlas);
     game.gameClock.start();
@@ -47,7 +52,7 @@ class GamePanel extends WebComponent {
     });
     _gameLoop.start();
   }
-  
+
   void enableButtons() {
     _endButton.disabled = false;
     _pauseButton.disabled = false;
@@ -57,13 +62,13 @@ class GamePanel extends WebComponent {
     _endButton.disabled = true;
     _pauseButton.disabled = true;
   }
-  
+
   void endGame() {
     // XXX: should confirm first
     game.stop();
-    
+
     disableButtons();
-    
+
     print('GAME ENDED');
   }
 
@@ -77,7 +82,7 @@ class GamePanel extends WebComponent {
     }
     paused = !paused;
   }
-  
+
   void drawCircle(int x, int y) {
     var context = _canvasElement.context2d;
     context.beginPath();
@@ -86,11 +91,85 @@ class GamePanel extends WebComponent {
     context.fill();
   }
 
+  String _keyboardSearchString = '';
+
+  String translateKeyboardButtonId(int buttonId) {
+    if (buttonId >= Keyboard.A && buttonId <= Keyboard.Z) {
+      return new String.fromCharCode(buttonId);
+    }
+    return '';
+  }
+
+  bool keyboardEventInterceptor(DigitalButtonEvent event, bool repeat) {
+    if (repeat == true) {
+      print('Repeat');
+      return true;
+    }
+    if (event.down == false) {
+      return true;
+    }
+    if (event.buttonId == Keyboard.ESCAPE ||
+        event.buttonId == Keyboard.SPACE) {
+      // Space or escape kills the current word search.
+      // TODO: Indicate in GUI.
+      _keyboardSearchString = '';
+      print('Cleared');
+      return true;
+    }
+    if (event.buttonId == Keyboard.ENTER) {
+      // Submit.
+      game.attemptWord(_keyboardSearchString);
+      _keyboardSearchString = '';
+      print('Cleared');
+      return true;
+    }
+    String newSearchString = _keyboardSearchString +
+                             translateKeyboardButtonId(event.buttonId);
+    if (event.buttonId < Keyboard.A || event.buttonId > Keyboard.Z) {
+      print('Invalid character.');
+      return true;
+    }
+    print(newSearchString);
+    print(event.buttonId);
+    if (game.stringInGrid(newSearchString, null)) {
+      print('String in grid.');
+      _keyboardSearchString = newSearchString;
+    } else if (event.buttonId == Keyboard.Q &&
+               game.stringInGrid(newSearchString + 'U', null)) {
+      print('Found for QU.');
+      _keyboardSearchString = newSearchString;
+    } else {
+      print('Here');
+      while (_keyboardSearchString.length > 0) {
+        if (_keyboardSearchString[_keyboardSearchString.length-1] == 'Q') {
+          _keyboardSearchString =
+              _keyboardSearchString.substring(0,_keyboardSearchString.length-1);
+        } else {
+          break;
+        }
+      }
+    }
+    print(_keyboardSearchString);
+    // Letter.
+    return true;
+  }
+
+
+  void gameUpdateKeyboard() {
+  }
+
+
   void gameUpdate(GameLoopHtml gameLoop) {
-    game.board.update(currentTouch);
+    if (TouchEvent.supported) {
+      // Only support touch on touch enabled devices.
+      game.board.update(currentTouch);
+    } else {
+      gameUpdateKeyboard();
+    }
   }
 
   void gameRender(GameLoopHtml gameLoop) {
+    game.selectSearchString(_keyboardSearchString);
     if (game != null) {
       game.board.render();
     }
--- lib/src/game.dart
+++ lib/src/game.dart
@@ -12,8 +12,11 @@ class Game {
     'P': 3, 'QU': 10, 'R': 1, 'S': 1, 'T': 1,
     'U': 1, 'V': 4,'W': 4, 'X': 8, 'Y': 4, 'Z': 10};
 
+  int multiplier = 1;
+  final List<int> letterBonusTiles = new List<int>(3);
+  final int wordBonusTile;
+
   var grid = new List.generate(4, (_) => new List<String>(4));
-  List selectedPositions = [];
   int score = 0;
   final Dictionary dictionary;
   Set<String> words = new Set<String>();
@@ -24,40 +27,13 @@ class Game {
   GameClock gameClock;
   BoardView board;
 
-  String get currentWord {
-    return selectedPositions.join('');
- }
 
-  void clearSelectedPositions() {
-    selectedPositions = [];
-  }
-
-  bool addToSelectedPositions(position) {
-    if (selectedPositions.isEmpty || this.validMove(selectedPositions.last, position)) {
-      selectedPositions.add(position);
-      return true;
-    }
-    return false;
-  }
-
-  bool isPositionSelected(position) {
-    bool selected = false;
-    for (var i = 0; i < selectedPositions.length; i++) {
-      if (selectedPositions[i].first == position.first &&
-          selectedPositions[i].last == position.last) {
-        selected = true;
-        break;
-      }
-    }
-    return selected;
-  }
-  
   Game(this.dictionary, this.canvas, gameLoop, this.letterAtlas) {
     _assignCharsToPositions();
     board = new BoardView(this, canvas);
     gameClock = new GameClock(gameLoop);
   }
-  
+
   void stop() {
     gameClock.stop();
   }
@@ -109,6 +85,106 @@ class Game {
         !(position1.x == position2.x && position1.x == position2.x);
   }
 
+
+
+  List<String> convertStringToTileString(String str) {
+    List<String> tileString = [];
+    for (int i = 0; i < str.length; i++) {
+      if ((str[i] == 'Q' && i+1 >= str.length) ||
+          (str[i] == 'Q' && str[i+1] != 'U')) {
+        // Q must always be followed by a U.
+        return [];
+      }
+      if (str[i] == 'Q') {
+        tileString.add('QU');
+        i++;
+      } else {
+        tileString.add(str[i]);
+      }
+    }
+    return tileString;
+  }
+
+  bool _findInGridWorker(List<String> tiles, int index, int i, int j,
+                         List<List<bool>> visited, List<int> path,
+                         Set<List<int>> paths) {
+    // Do bounds check.
+    if (i < 0 || j < 0 || i >= 4 || j >= 4) {
+      return false;
+    }
+    if (visited[i][j] == true) {
+      return false;
+    }
+    if (grid[i][j] != tiles[index]) {
+      return false;
+    }
+    path.add(i*4+j);
+    if (tiles.length == index+1) {
+      // Valid.
+      paths.add(new List.from(path));
+      path.removeLast();
+      return true;
+    }
+    visited[i][j] = true;
+    // DFS.
+    bool r = false;
+    // Left side.
+    r = r || _findInGridWorker(tiles, index+1, i-1, j-1, visited, path, paths);
+    r = r || _findInGridWorker(tiles, index+1, i-1, j, visited, path, paths);
+    r = r || _findInGridWorker(tiles, index+1, i-1, j+1, visited, path, paths);
+    // Right side.
+    r = r || _findInGridWorker(tiles, index+1, i+1, j-1, visited, path, paths);
+    r = r || _findInGridWorker(tiles, index+1, i+1, j, visited, path, paths);
+    r = r || _findInGridWorker(tiles, index+1, i+1, j+1, visited, path, paths);
+    // Top and bottom.
+    r = r || _findInGridWorker(tiles, index+1, i, j-1, visited, path, paths);
+    r = r || _findInGridWorker(tiles, index+1, i, j+1, visited, path, paths);
+    visited[i][j] = false;
+    path.removeLast();
+    return r;
+  }
+
+  bool _findInGridAt(int i, int j, List<String> tiles, Set<List<int>> paths) {
+    var visited = new List.generate(4, (_) => new List<bool>(4));
+    for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < 4; j++) {
+        visited[i][j] = false;
+      }
+    }
+    return _findInGridWorker(tiles, 0, i, j, visited, [], paths);
+  }
+
+  bool stringInGrid(String search, Set<List<int>> paths) {
+    List<String> tileStrings = convertStringToTileString(search);
+    // Not there.
+    if (tileStrings.length == 0) {
+      return false;
+    }
+    if (paths == null) {
+      paths = new Set<List<int>>();
+    }
+    bool r = false;
+    for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < 4; j++) {
+        bool v = _findInGridAt(i, j, tileStrings, paths);
+        r = r || v;
+      }
+    }
+    return r;
+  }
+
+  void selectSearchString(String searchString) {
+    Set<List<int>> paths = new Set<List<int>>();
+    board.selectedTiles.clear();
+    if (stringInGrid(searchString, paths)) {
+      paths.forEach((path) {
+        for (int i = 0; i < path.length; i++) {
+          board.selectedTiles.add(path[i]);
+        }
+      });
+    }
+  }
+
   bool attemptWord(String word) {
     if (words.contains(word)) {
       return false;
@@ -123,7 +199,7 @@ class Game {
   }
 
   int scoreForWord(String word) {
-    List<int> scores = word.split('').map(
+    List<int> scores = convertStringToTileString(word).map(
         (char) => Game.LETTERS[char]).toList();
     return scores.reduce((value, element) => value + element);
   }
