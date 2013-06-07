ae35674da25930c26acd5899b571e3e32237735b
adam <financeCoding@gmail.com>
2013-06-07 11:54:46 -0700

    Updating login panel to bindin auth context, signed in, signed out.

--- lib/components/login_panel.dart
+++ lib/components/login_panel.dart
@@ -1,11 +1,68 @@
 import 'dart:html';
-
+import 'dart:json' as JSON;
 import "package:js/js.dart" as js;
 import 'package:web_ui/web_ui.dart';
 import 'package:tilebasedwordsearch/tilebasedwordsearch.dart' as app;
+import "package:google_oauth2_client/google_oauth2_browser.dart";
+
+typedef OnSignInCallback(SimpleOAuth2 authenticationContext);
+typedef OnSignOutCallback();
 
 class LoginPanel extends WebComponent {
+  OnSignInCallback signInCallback;
+  OnSignOutCallback signOutCallback;
+  SimpleOAuth2 authenticationContext;
+
+  _onSignInCallback(Map authResult) {
+    print(authResult);
+
+    if (authResult["access_token"] != null) {
+      query("#google-connect").style.display = "none";
+      query("#auth-disconnect").style.display = "block";
+
+      // Enable Authenticated requested with the granted token in the client libary
+      authenticationContext.token = authResult["access_token"];
+      authenticationContext.tokenType = authResult["token_type"];
+
+      // Notify
+      if (signInCallback != null) {
+        signInCallback(authenticationContext);
+      }
+    } else if (authResult["error"] != null) {
+      print("There was an error: ${authResult["error"]}");
+      query("#google-connect").style.display = "block";
+      query("#auth-disconnect").style.display = "none";
+    }
+  }
+
+  /**
+   * Calls the OAuth2 endpoint to disconnect the app for the user.
+   */
+  void disconnect(event) {
+    js.scoped(() {
+      // JSONP workaround because the accounts.google.com endpoint doesn't allow CORS
+      js.context.myJsonpCallback = new js.Callback.once(([jsonData]) {
+        print("revoke response: $jsonData");
+        query("#auth-disconnect").style.display = "none";
+        query("#google-connect").style.display = "block";
+
+        // disable authenticated requests in the client library
+        authenticationContext.token = null;
+
+        if (signOutCallback != null) {
+          signOutCallback();
+        }
+      });
+
+      ScriptElement script = new Element.tag("script");
+      script.src = "https://accounts.google.com/o/oauth2/revoke?token=${authenticationContext.token}&callback=myJsonpCallback";
+      document.body.children.add(script);
+    });
+  }
+
   void created() {
+    //authenticationContext = new SimpleOAuth2(null);
+
     /**
      * Calls the method that handles the authentication flow.
      *
@@ -13,8 +70,18 @@ class LoginPanel extends WebComponent {
      *   other authentication information.
      */
     js.scoped(() {
+      var reviverOAuth = new js.Callback.many((key, value) {
+        if (key == "g-oauth-window") {
+          return "";
+        }
+
+        return value;
+      });
+
       js.context.onSignInCallback =  new js.Callback.many((js.Proxy authResult) {
-        print("authResult = ${authResult}");
+        Map dartAuthResult =
+            JSON.parse(js.context.JSON.stringify(authResult, reviverOAuth));
+        _onSignInCallback(dartAuthResult);
       });
     });
 
