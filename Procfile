77856bcf781afc2bf0d5042ab9d1ec5247aae254
Seth Ladd <sethladd@google.com>
2013-06-07 12:04:00 -0700

    add procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: /app/dart-sdk/bin/dart bin/main.dart
--- bin/server.dart
+++ bin/server.dart
@@ -8,8 +8,13 @@ Connection db;
 
 void main() {
   var port = int.parse(Platform.environment['PORT'], onError: (_) => 8080);
-  var dbUrl = Platform.environment['DATABASE_URL'] != null ?
-      Platform.environment['DATABASE_URL'] : 'postgres://sethladd:@localhost:5432/sethladd';
+  var dbUrl;
+  if (Platform.environment['DATABASE_URL'] != null) {
+    dbUrl = Platform.environment['DATABASE_URL'];
+  } else {
+    var user = Platform.environment['USER'];
+    dbUrl = 'postgres://$user:@localhost:5432/$user';
+  }
   
   connect(dbUrl)
   .then((conn) => db = conn)
@@ -17,34 +22,35 @@ void main() {
     print('DB connected, now starting up web server');
     return start(public: 'web', host: '0.0.0.0', port: port).then((app) {
       print('HTTP server started');
-      app.post('/cats', createCat);
-      app.get('/cats', listCats);
+      app.get('/games/:id', getGame);
+      //app.get('/games/mine', myGames);
     });
   })
   .catchError((e) => print("error: $e"));
 }
 
-listCats(Request req, Response res) {
-  db.query('SELECT * FROM cats').map((row) => row.name).toList().then((list) {
-    res.json(list);
+getGame(Request req, Response res) {
+  var id = req.params['id'];
+  db.query('SELECT * FROM games WHERE id = @id', {'id': id}).first.then((game) {
+    res.json(game);
   });
 }
 
-createCat(Request req, Response res) {
-  HttpBodyHandler.processRequest(req.input)
-    .then((HttpBody body) {
-      return body.body['name'];
-    })
-    .then((name) => db.execute('INSERT INTO cats (name) VALUES (@n)', {'n':name}))
-    .then((_) {
-      res
-       ..status(201)
-       ..close();
-    })
-    .catchError((e) {
-      print("Error with insert: $e");
-      res
-        ..status(500)
-        ..close();
-    });
-}
+//createCat(Request req, Response res) {
+//  HttpBodyHandler.processRequest(req.input)
+//    .then((HttpBody body) {
+//      return body.body['name'];
+//    })
+//    .then((name) => db.execute('INSERT INTO cats (name) VALUES (@n)', {'n':name}))
+//    .then((_) {
+//      res
+//       ..status(201)
+//       ..close();
+//    })
+//    .catchError((e) {
+//      print("Error with insert: $e");
+//      res
+//        ..status(500)
+//        ..close();
+//    });
+//}
