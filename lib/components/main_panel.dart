3dfd52e09a2f5ec0a0caf980ca98c2b212c55037
Shailen Tuli <shailen.tuli@gmail.com>
2013-06-25 07:24:41 -0700

    Panel renaming; made word and score CSS reusable

--- lib/components/main_panel.dart
+++ lib/components/main_panel.dart
@@ -1,11 +1,6 @@
 import 'package:web_ui/web_ui.dart';
 import 'package:tilebasedwordsearch/tilebasedwordsearch.dart' as app;
 
-// Main landing page for the game.
-// Includes:
-// - "start new" button
-// - "inprogress" list
-// - "highscores" button or reference to panel.
 class MainPanel extends WebComponent {
 
 }
--- lib/components/recent_words_panel.dart
+++ lib/components/recent_words_panel.dart
@@ -2,8 +2,8 @@ import 'package:web_ui/web_ui.dart';
 import 'package:tilebasedwordsearch/tilebasedwordsearch.dart' as app;
 
 @observable
-class WordListComponent extends WebComponent {
-  
+class RecentWordsPanel extends WebComponent {
+
   String scoreFor(int index) {
     return app.board.words[app.board.recentWords[index]].toString();
   }
