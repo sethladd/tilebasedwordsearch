544d32a9f11411ea0d5f9ec38250d2edfff3d8d3
Seth Ladd <sethladd@gmail.com>
2013-06-25 23:37:28 -0700

    remove weird line

--- tool/deploy_heroku.sh
+++ tool/deploy_heroku.sh
@@ -8,7 +8,6 @@ pub install
 dart build.dart 
 cd web/out/
 dart2js --verbose -oindex.html_bootstrap.dart.js index.html_bootstrap.dart
-index.html_bootstrap.dart
 cd ../../
 git add .gitignore
 git add .
