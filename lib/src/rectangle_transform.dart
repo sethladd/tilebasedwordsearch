de4024a6768c92794c38b89943a1b1fe4e6fd8c0
John McCutchan <john@johnmccutchan.com>
2013-06-26 10:56:27 -0700

    Remove pubspec.lock

--- lib/src/rectangle_transform.dart
+++ lib/src/rectangle_transform.dart
@@ -41,6 +41,22 @@ class RectangleTransform {
            top+yChop <= y && y <= bottom-yChop;
   }
 
+  // Return < 0
+  double lineSide(double x0, double y0, double x1, double y1,
+                  double x, double y) {
+    return (y1-y0)*x + (x0-x1)*y + (x1*y0-x0*y1);
+  }
+
+  bool _containsLine(double x0, double y0, double x1, double y1,
+                     double l, double r, double t, double b) {
+  }
+
+  bool containsLine(int x0, int y0, int x1, int y1) {
+    return _containsLine(x0.toDouble(), y0.toDouble(), x1.toDouble(),
+                         y1.toDouble(), left.toDouble(), right.toDouble(),
+                         top.toDouble(), bottom.toDouble());
+  }
+
   /// Transform [x] into the [this] coordinate system.
   int transformX(int x) => x - left;
   /// Transform [y] into the [this] coordinate system.
