6acc9ac1bb930ce4dfa7219f2330fd802244617b
Seth Ladd <sethladd@google.com>
2013-10-24 16:57:12 -0700

    update the schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS player;
 DROP TABLE IF EXISTS match;
 DROP TABLE IF EXISTS twoplayermatch;
 
-CREATE TABLE IF NOT EXISTS match (
+CREATE TABLE IF NOT EXISTS gamematch (
   id SERIAL,
   p1_id VARCHAR(255),
   p2_id VARCHAR(255),
@@ -15,13 +15,13 @@ CREATE TABLE IF NOT EXISTS match (
   p2_game TEXT
 );
 
+CREATE INDEX ON gamematch (p1_id);
+CREATE INDEX ON gamematch (p2_id);
+
 CREATE TABLE IF NOT EXISTS player (
   id SERIAL,
   gplus_id VARCHAR(255) UNIQUE,
   name VARCHAR(255)
 );
-
-CREATE INDEX ON match (p1_id);
-CREATE INDEX ON match (p2_id);
 
 INSERT INTO player (gplus_id, name) VALUES ('100399120809275930649', 'Kathy Walrath');
