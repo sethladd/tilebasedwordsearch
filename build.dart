4d639777d848069aa9ef8e7c959bb9cbe3231be8
Seth Ladd <sethladd@google.com>
2013-10-28 23:13:21 -0700

    remove old scripts, update README, make one build.dart

--- build.dart
+++ build.dart
@@ -1,39 +1,27 @@
 #!/usr/bin/env dart
 
 import 'package:polymer/builder.dart';
+import 'dart:io';
 
 void main() {
-  lint(entryPoints: ['web/index.html']);
+  build(entryPoints: ['web/index.html'])
+  //.then((_) => deploy(entryPoints: ['web/index.html']))
+  .then((_) {
+    if (parseOptions().forceDeploy) {
+      compileToJs();
+    }
+  });
 }
 
-//generateAppCache() {
-//  var file = new File('web/out/appcache.manifest');
-//  if (!file.existsSync()) {
-//    file.createSync();
-//  }
-//  
-//  IOSink out = file.openWrite();
-//  out.writeln('CACHE MANIFEST');
-//  out.writeln('# ${new DateTime.now()}');
-//  out.writeln('\nCACHE:');
-//  
-//  new Directory('web/out')
-//      .listSync(recursive: true, followLinks: false)
-//      .forEach((entry) {
-//        if (entry is Link || entry.path.endsWith('.map') ||
-//            entry.path.contains('_from_packages') ||
-//            entry is Directory || entry.path.endsWith('appcache.manifest')) {
-//          return;
-//        }
-//        out.writeln(entry.path);
-//      });
-//  
-//  out.writeln('web/out/index.html_bootstrap.dart.js');
-//  out.writeln('packages/browser/dart.js');
-//  out.writeln('packages/browser/interop.js');
-//  out.writeln('packages/js/dart_interop.js');
-//  
-//  out.writeln('\nNETWORK:');
-//  out.writeln('*');
-//  out.close();
-//}
+compileToJs() {
+  print('Running dart2js, assuming it is on the PATH');
+  var result =
+    Process.runSync('dart2js', [
+        '--minify',
+        '-o', 'out/web/index.html_bootstrap.dart.js',
+        'out/web/index.html_bootstrap.dart', '--suppress-hints'],
+        runInShell: true);
+  print("STDOUT: ${result.stdout}");
+  print("STDERR: ${result.stderr}");
+  print("Done compiling to JS");
+}
--- tool/local_server_build.sh
+++
@@ -1,1 +0,0 @@
-dart build.dart && cd web/out && dart2js --minify -oindex.html_bootstrap.dart.js index.html_bootstrap.dart && cd ../../
