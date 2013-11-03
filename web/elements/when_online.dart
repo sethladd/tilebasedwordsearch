c66b6273e404e3a2699654684037081410431097
Seth Ladd <sethladd@google.com>
2013-11-02 18:00:29 -0700

    detect online/offline

---
+++ web/elements/when_online.dart
@@ -0,0 +1,24 @@
+import 'package:polymer/polymer.dart';
+import 'dart:html';
+
+/**
+ * TODO actually try to reach the server, which tests the network link.
+ */
+@CustomTag('when-online')
+class WhenOnline extends PolymerElement {
+  @observable bool isOnline = false;
+
+  WhenOnline.created() : super.created();
+
+  @override
+  void enteredView() {
+    super.enteredView();
+
+    if (window.navigator.onLine) {
+      isOnline = true;
+    }
+
+    window.onOnline.listen((_) => isOnline = true);
+    window.onOffline.listen((_) => isOnline = false);
+  }
+}
